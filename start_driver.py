cb57de88f1545f9a28b6134b1bcdd81b7fe836e8
madittman <mdtn@gmx.de>
2025-06-27 14:08:35 +0200

    Fix bug in argparse

--- start_driver.py
+++ start_driver.py
@@ -10,8 +10,8 @@ parser.add_argument("num_of_map_tasks", type=int, help="Number of map tasks")
 parser.add_argument("num_of_reduce_tasks", type=int, help="Number of reduce tasks")
 parser.add_argument("file_path", type=str, help="File path to look for")
 args = parser.parse_args()
-if args.num_of_reduce_tasks < args.num_of_map_tasks:
-    raise TypeError("Number of reduce tasks cannot be less than number of map tasks")
+if args.num_of_map_tasks < args.num_of_reduce_tasks:
+    raise TypeError("Number of map tasks cannot be less than number of reduce tasks")
 
 driver: Driver = Driver(
     num_of_map_tasks=args.num_of_reduce_tasks,
