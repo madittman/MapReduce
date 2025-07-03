666ee698b2526a86332167788b0000b6728af1ff
madittman <mdtn@gmx.de>
2025-07-03 14:23:43 +0200

    Improve word pattern matching in worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,6 @@
-import pprint
-
 import grpc
 import os
+import re
 from dataclasses import dataclass, field
 from time import sleep
 from typing import Dict, List, Union
@@ -20,10 +19,11 @@ class Worker:
     def _get_words_by_count(input_files: List[str]) -> Dict[str, int]:
         """Return map of each word by count."""
         words_by_count: Dict[str, int] = {}
+        pattern: str = r"[a-zA-Z0-9'-]+"
         for filename in input_files:
             with open(filename, "r") as file:
                 content: str = file.read()
-                words: List[str] = content.split()
+                words: List[str] = re.findall(pattern, content)
                 for word in words:
                     if word not in words_by_count.keys():
                         words_by_count[word] = 1
@@ -49,9 +49,10 @@ class Worker:
             bucket_id: int = ord(word[0]) % self.num_of_buckets
             buckets[bucket_id][word] = count
 
-        # Create folder for intermediate files
+        # Create folder for intermediate files if not already done by another worker
         intermediate_path: str = "./intermediate_files"
         if not os.path.exists(intermediate_path):
+            print(f"Worker {self.pid}: Creating intermediate folder")
             os.makedirs(intermediate_path)
 
         # Create and write words by count to intermediate files
@@ -62,10 +63,42 @@ class Worker:
                     file.write(f"{word} {count}\n")
 
     def _process_reduce_task(self, task_id: int, files: str) -> None:
+        """Sum up word counts from files and write to output file."""
         print(
             f"Worker {self.pid}: Processing reduce task, task_id = {task_id}, files = {files}\n"
         )
         sleep(2)  # for testing
+
+        # Read in all words by count from the intermediate files
+        words_by_count: Dict[str, int] = self._get_words_by_count(files)
+        for filename in files:
+            with open(filename, "r") as file:
+                for line in file:
+                    word: str
+                    count: str
+                    word, count = line.split()
+                    count: int = int(count)
+                    if word not in words_by_count.keys():
+                        words_by_count[word] = count
+                    else:
+                        words_by_count[word] += count
+        # Sort dictionary by descending word count order
+        words_by_count = dict(
+            sorted(words_by_count.items(), key=lambda item: item[1], reverse=True)
+        )
+
+        # Create folder for output files if not already done by another worker
+        output_path: str = "./output_files"
+        if not os.path.exists(output_path):
+            print(f"Worker {self.pid}: Creating output folder")
+            os.makedirs(output_path)
+
+        # Write word by count to output file.
+        # The output file is bound to the reduce task.
+        output_file: str = os.path.join(output_path, f"out-{task_id}")
+        with open(output_file, "w") as file:
+            for word, count in words_by_count.items():
+                file.write(f"{word} {count}\n")
 
     def run(self) -> None:
         with grpc.insecure_channel("localhost:50051") as channel:
